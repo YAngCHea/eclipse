1ac096be838d850522c8706eb4297a2042990c26
Administrator <Administrator@DESKTOP-RMA9FF4>
2023-08-14 10:32:58 +0900

    html06

---
+++ html06/src/main/java/com/java/www/S01.java
@@ -0,0 +1,34 @@
+package com.java.www;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S01
+ */
+@WebServlet("/S01")
+public class S01 extends HttpServlet {
+	
+    public S01() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("안녕하세요.doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("안녕하세요.doPost");
+	}
+
+}
